f2a8156ad1cb09cecb65c38f66bcb719a8682476
sumantasam1990 <sumantasam1990@gmail.com>
2017-01-01 21:07:08 +0530

    Update dbClass.php

--- dbClass.php
+++ dbClass.php
@@ -3,6 +3,7 @@
 *
 Class Name: DB Class
 Author Name: Sumanta Kundu
+Version: 1.0
 *
 */
 namespace DBClassNamespace;
@@ -11,21 +12,21 @@ namespace DBClassNamespace;
   private $user = 'root';
   private $password = '';
   private $dbname = 'shop';
-  
-  public $con;
-  public $table;
-  public $row = array();
-  public $data = array();
+
+  private $con;
+  private $query;
+  private $table;
+  private $row = array();
+  private $data = array();
 
   public function __construct() {
     $this->con = mysqli_connect($this->host,$this->user,$this->password,$this->dbname);
   }
-  
-  protected function selectResults($table) {
-    $tblName = $this->table = $table;
-    $sql = "SELECT * FROM $tblName";
-    $result = $this->con->query($sql);
 
+  protected function selectResults($query) {
+    $query = $this->query = $query;
+    $sql = "$query";
+    $result = $this->con->query($sql);
     if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
         $this->row[] = $row;
@@ -34,5 +35,30 @@ namespace DBClassNamespace;
     } else {
       return false;
     }
+  }
+
+  protected function insertData($table,$insertVal=array()) {
+    $this->table = $table;
+    $columns = '';
+    $instvalue = '';
+    //extract value from the associative array
+    foreach (array_keys($insertVal) as $column) {
+      $columns .= $column . ', ';
+    }
+    //get value from $insertVal array()
+    foreach ($insertVal as $key => $value) {
+      $instvalue .= '"' . addslashes($value) .'"' . ', ';
+    }
+    //remove comma from last key
+    $insertColumns = substr($columns, 0, -2);
+    $values = substr($instvalue, 0, -2);
+    //main insert query
+    $stmt = $this->con->query("INSERT INTO $this->table ($insertColumns) VALUES ($values)");
+    if( $stmt ) {
+      return TRUE;
+    } else {
+      return False;
+    }
+    $this->con->close();
   }
 }
